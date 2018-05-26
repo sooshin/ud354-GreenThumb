bfb22669e25c33576a0b7d3911277b775eeb9c6c
Soojeong <soojeongshin2@gmail.com>
2018-05-26 21:50:35 +0900

    Implement Log AddToCart Event

---
+++ app/src/main/java/com/google/firebase/udacity/greenthumb/Analytics.java
@@ -0,0 +1,23 @@
+package com.google.firebase.udacity.greenthumb;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.udacity.greenthumb.data.Plant;
+
+public class Analytics {
+
+    public static void logEventAddToCart(Context context, Plant plant, int quantity) {
+        Bundle params = new Bundle();
+        params.putInt(FirebaseAnalytics.Param.ITEM_ID, plant.id);
+        params.putString(FirebaseAnalytics.Param.ITEM_NAME, plant.name);
+        params.putString(FirebaseAnalytics.Param.ITEM_CATEGORY, "plants");
+        params.putDouble(FirebaseAnalytics.Param.QUANTITY, quantity);
+        params.putDouble(FirebaseAnalytics.Param.PRICE, plant.price);
+
+        FirebaseAnalytics.getInstance(context).logEvent(
+                FirebaseAnalytics.Event.ADD_TO_CART, params);
+    }
+}
+
